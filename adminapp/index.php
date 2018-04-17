05eb44b406ae3f7d3c311724256b76be8081517b
chenjunxu <chenjunxu@meizu.com>
2018-04-17 22:02:19 +0800

    fix adminapp

--- adminapp/index.php
+++ adminapp/index.php
@@ -9,21 +9,16 @@
 //应用路径
 define('APP_ROOT', dirname(__FILE__));
 //基础路径  （注意 不是框架路径）
-define('BASE_ROOT', dirname(__FILE__));
+define('BASE_ROOT', dirname(dirname(__FILE__)));
 //基础路径  （注意 不是框架路径）
-define('SITE_ROOT', dirname(dirname(__FILE__)));
-
-//应用个性化配置
-//域名
-define('DOMAIN', 'https://www.shop.com/');
+//define('SITE_ROOT', dirname(dirname(__FILE__)));
 
 
 //获取配置
-BASE_ROOT.'config/main.php';
 $config = include(BASE_ROOT.'/config/main.php');
 
 //调用框架
-$nice  = BASE_ROOT.'/../framework/nice.php';
+$nice  = BASE_ROOT.'/framework/nice.php';
 
 
 require_once($nice);
